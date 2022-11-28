1ec8152fd918d2b025bed49b24e9dbdf59b36ab0
PRATHAMESH SHUKLA <53365574+pratham-0709@users.noreply.github.com>
2022-11-28 17:36:24 +0530

    Aws Ami Creation

---
+++ Aws-Ec2/aws_ami.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key = "Your AWS Account Access key"
+  secret_key = "Your AWS Account Secret Key"
+}
+
+resource "aws_ami" "example" {
+  name                = "terraform-example"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-xxxxxxxx"
+    volume_size = 8
+  }
+}
